17eb09167b34d82bf8032edac2d9b698c76600dc
Piyush Punera <piyushpunera107@gmail.com>
2026-08-06 19:09:43 +0530

    feat: add premium hover micro-interactions and smooth lenis scroll easing

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 // Initialize Lenis
 const lenis = new Lenis({
-  duration: 1.2,
+  duration: 1.5,
   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
   direction: 'vertical',
   gestureDirection: 'vertical',
