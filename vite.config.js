3e64d2979a57d7fe8172689f67427ddba3f80369
Piyush-031005 <piyushpunera107@gmail.com>
2026-09-24 00:02:15 +0530

    fix(build): Fix manualChunks warning that crashes Vercel CI build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
   assetsInclude: ['**/*.glsl', '**/*.gltf', '**/*.glb'],
 })
